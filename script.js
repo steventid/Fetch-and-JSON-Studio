ff8d2c0e9238ba9bca05a67c1a0c452860b158e3
Steven Tidwell <steventid1313@gmail.com>
2022-09-13 18:36:42 -0500

    Updated site per LC101 Mission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // TODO: add code here
+window.addEventListener("load", function() {
+    let json = [];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(function(json) {
+            json.sort(function(b, a){return a.hoursInSpace - b.hoursInSpace});
+            let html = "";
+            for(a of json) {
+                let green = "";
+                if (a.active) {
+                    green = 'class="active"';
+                }
+                html += `<div class="astronaut">
+                <div class="bio">
+                   <h3>${a.firstName} ${a.lastName}</h3>
+                   <ul>
+                      <li>Hours in space: ${a.hoursInSpace}</li>
+                      <li ${green}>Active: ${a.active}</li>
+                      <li>Skills: ${a.skills.join(', ')}</li>
+                   </ul>
+                </div>
+                <img class="avatar" src="${a.picture}">
+             </div>`
+            
+            };
+            document.getElementById("main").innerHTML = `Astronauts: ${json.length}`;
+            document.getElementById("container").innerHTML = html;
+        });
+    });
+});
